c767573eec2ac487f7efbd2c50cccbbdb5ef27e7
bogdanstef <bogdanstefoni.wd@gmail.com>
2020-03-25 12:34:31 +0200

    added PetTypse in with Bootstrap startup . Closes #29

--- pet-clinic-web/src/main/java/stefoni/springframework/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/stefoni/springframework/bootstrap/DataLoader.java
@@ -3,8 +3,10 @@ package stefoni.springframework.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import stefoni.springframework.model.Owner;
+import stefoni.springframework.model.PetType;
 import stefoni.springframework.model.Vet;
 import stefoni.springframework.services.OwnerService;
+import stefoni.springframework.services.PetTypeService;
 import stefoni.springframework.services.VetService;
 
 @Component
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
